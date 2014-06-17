99ff7fc756fd1b79a9dd6167ddfec44a820f3152
Алексей Овчинников <alexey.ovchinnikov@yandex.ru>
2014-06-17 22:02:00 +0400

    some improvements

--- anyloader.js
+++ anyloader.js
@@ -15,56 +15,69 @@
   'use strict';
 
   var SELECTOR = '[name],[id],[data-name]';
-  var NON_URL = /[{}<>\[\]]/;
+  var NON_URI = /[{}<>\[\]]/;
+  var URI = /^(?:(https?:\/\/.*)|(?:url\(\s*'([^']*)'\s*\))|(?:url\(\s*"([^"]*)"\s*\))|(?:url\(\s*([^)]*)\s*\)))/;
 
   var isDeferred = function(obj) {
     return (obj instanceof $.Deferred) || _.isFunction(obj.then);
   };
 
-  return function(methods, context) {
-    if (!_.isObject(methods)) methods = {};
+  return function(options, context) {
+    if (!_.isObject(options)) options = {};
 
     var load = function() {
-      // get function arguments
-      var args = Array.prototype.slice.call(arguments, 0);
       var result = [];
 
-      _.each(args, function(arg) {
+      _.each(_.toArray(arguments), function(arg) {
         if (arg == null) return; // next argument
         arg = load.parse.apply(load.context, [arg]);
         if (arg == null) return; // next argument
 
         if (!isDeferred(arg)) {
+          // if parsed argument is array and we have create:element callback
           if (_.isArray(arg) && _.isFunction(load['create:element'])) {
+            // create master deferred for all array elements
             var deferred = $.Deferred();
             $.when
               .apply(load.context, _.map(arg, load['create:element'], load.context))
               .done(function() {
+                // get all processed array elements in arr
                 var arr = _.toArray(arguments);
+                // if we have create callback, call it for entire result array
                 if (_.isFunction(load.create)) arr = load.create.apply(load.context, [arr]);
+                // and resolve master deferred
                 deferred.resolve(arr);
               });
             result.push(deferred);
             return; // next argument;
           }
 
-          var childFactories = _.filter(_.keys(arg), function(key) {
-            return _.isFunction(load['create:' + key]);
-          });
-          if (childFactories.length > 0) {
-            var deferred = $.Deferred();
-            var childValues = _.map(childFactories, function(key) {
-              return load['create:' + key].apply(load.context, [arg[key]])
+          // if parsed argument is plain object
+          if (_.isObject(arg)) {
+            // get a list of object keys for which we have appropriate callback
+            var hasCallback = _.filter(_.keys(arg), function(key) {
+              return _.isFunction(load['create:' + key]);
             });
-            $.when
-              .apply(load.context, childValues)
-              .done(function() {
-                _.extend(arg, _.object(childFactories, _.toArray(arguments)));
-                if (_.isFunction(load.create)) arg = load.create.apply(load.context, [arg]);
-                deferred.resolve(arg);
+            if (hasCallback.length > 0) {
+              // if we have at least one specific callback - create master deferred
+              var deferred = $.Deferred();
+              // parse all values with callbacks
+              var values = _.map(hasCallback, function(key) {
+                return load['create:' + key].apply(load.context, [arg[key]])
               });
-            result.push(deferred);
-            return;
+              $.when
+                .apply(load.context, values)
+                .done(function() {
+                  // get all processed keys in result object
+                  _.extend(arg, _.object(hasCallback, _.toArray(arguments)));
+                  // if we have create callback, call it for entire result object
+                  if (_.isFunction(load.create)) arg = load.create.apply(load.context, [arg]);
+                  // and resolve master deferred
+                  deferred.resolve(arg);
+                });
+              result.push(deferred);
+              return;
+            }
           }
 
           if (_.isFunction(load.create)) {
@@ -79,7 +92,11 @@
       return $.when.apply(load.context, result);
     };
 
-    load.defaults = {
+    load.parsers = {
+      'parse:string': function(arg) {
+        return _.isString(arg) ? arg : null;
+      },
+
       'parse:json': function(arg) {
         try {
           return $.parseJSON(arg.replace(/[\n\r\t]/g, ''));
@@ -101,7 +118,10 @@
 
       'parse:uri': function(arg) {
         var deferred = $.Deferred();
-        $.ajax({ url: arg }).done(function(text, status) {
+        var uri = arg.match(URI);
+        if (uri == null) return null;
+        uri = uri[1] || uri[2] || uri[3] || uri[4]
+        $.ajax({ url: uri }).done(function(text, status) {
           if (status == 'success') {
             // result should be an object (in case of JSON file) or HTML
             if (_.isObject(text) || _.isString(text)) {
@@ -112,13 +132,16 @@
                 deferred.resolve(result);
               }
             } else {
-              deferred.reject('wrong object in ' + arg);
+              // deferred.reject('wrong object in ' + arg);
+              deferred.resolve(arg);
             }
           } else {
-            deferred.reject('error while loading object from ' + arg);
+            // deferred.reject('error while loading object from ' + arg);
+            deferred.resolve(arg);
           }
         }).fail(function() {
-          deferred.reject('error while loading object from ' + arg);
+          // deferred.reject('error while loading object from ' + arg);
+          deferred.resolve(arg);
         });
         return deferred;
       },
@@ -135,33 +158,38 @@
         return (_.isEmpty(obj)) ? arr : obj;
       },
 
+      'parse:array': function(arg) { return arg; },
+
+      'parse:object': function(arg) { return arg; },
+
       parse: function(arg) {
         if (arg == null) return null; // next argument
-
         if (_.isString(arg)) {
-          if (NON_URL.test(arg)) {
+          if (NON_URI.test(arg)) {
             // argument is JSON or HTML string
             arg = load['parse:json'].apply(load.context, [arg]) ||
                   load['parse:html'].apply(load.context, [arg]);
           } else {
             // argument is URI string
-            arg = load['parse:uri'].apply(load.context, [arg]);
+            arg = load['parse:uri'].apply(load.context, [arg]) ||
+                  load['parse:string'].apply(load.context, [arg]);
             if (isDeferred(arg)) return arg;
           }
         }
 
         if (arg == null) return null; // next argument
+        if (arg instanceof $) arg = load['parse:jquery'].apply(load.context, [arg]);
 
-        if (arg instanceof $) {
-          // argument is jQuery object
-          return load['parse:jquery'].apply(load.context, [arg]);
-        };
+        if (arg == null) return null; // next argument
+        if (_.isArray(arg)) arg = load['parse:array'].apply(load.context, [arg]);
+
+        if (_.isObject(arg)) arg = load['parse:object'].apply(load.context, [arg]);
 
         return arg;
       }
     };
 
-    _.extend(load, load.defaults, methods);
+    _.extend(load, load.parsers, options);
     load.context = context || load;
 
     return load;
--- test/spec/anyloader-spec.js
+++ test/spec/anyloader-spec.js
@@ -21,21 +21,21 @@
       });
     });
 
-    describe('loading from HTML string', function() {
-      it('invokes parse callback if it given', function(done) {
-        LoaderFactory({ parse: function(text) {
-          expect(text).to.be('<i class="test"></i>');
-          return { f1: 'f1t' };
-        }})('<i class="test"></i>').done(function(obj) {
-          expect(obj).to.eql({ f1: 'f1t' });
-          done();
-        });
+    it('invokes parse callback if it given', function(done) {
+      LoaderFactory({ parse: function(text) {
+        expect(text).to.be('<i class="test"></i>');
+        return { f1: 'f1t' };
+      }})('<i class="test"></i>').done(function(obj) {
+        expect(obj).to.eql({ f1: 'f1t' });
+        done();
       });
+    });
 
-      it('invokes parseHTML callback if it given', function(done) {
+    describe('loading from HTML string', function() {
+      it('invokes parse:html callback if it given', function(done) {
         var loader = LoaderFactory({ 'parse:html': function(text) {
           expect(text).to.be('<i class="test"></i>');
-          return this.defaults['parse:html']('<i name="f1">f1t</i>');
+          return this.parsers['parse:html']('<i name="f1">f1t</i>');
         }});
         loader('<i class="test"></i>').done(function(obj) {
           expect(obj).to.eql({ f1: 'f1t' });
@@ -75,10 +75,10 @@
     });
 
     describe('loading from JSON string', function() {
-      it('invokes parseJSON callback if it given', function(done) {
+      it('invokes parse:json callback if it given', function(done) {
         var loader = LoaderFactory({ 'parse:json': function(text) {
           expect(text).to.be('{"f": "ft"}');
-          return loader.defaults['parse:json']('{"f1": "f1t"}');
+          return loader.parsers['parse:json']('{"f1": "f1t"}');
         }});
         loader('{"f": "ft"}').done(function(obj) {
           expect(obj).to.eql({ f1: 'f1t' });
@@ -101,30 +101,61 @@
       });
     });
 
-    describe('loading from URI string', function() {
-      it('fails on wrong URI', function(done) {
-        LoaderFactory()('wrong.uri').fail(function(msg) {
-          expect(msg).to.eql('error while loading object from wrong.uri');
+    describe('loading string itself', function() {
+      it('invokes parse:string callback if it given', function(done) {
+        var loader = LoaderFactory({ 'parse:string': function(text) {
+          expect(text).to.be('test');
+          return { f1: 'f1t' };
+        }});
+        loader('test').done(function(obj) {
+          expect(obj).to.eql({ f1: 'f1t' });
           done();
         });
       });
 
-      it('fails on wrong content of downloaded file', function(done) {
-        LoaderFactory()('data/wrong-data.txt').fail(function(msg) {
-          expect(msg).to.eql('wrong object in data/wrong-data.txt');
+      it('parses string as is', function(done) {
+        LoaderFactory()('string').done(function(obj) {
+          expect(obj).to.eql('string');
+          done();
+        });
+      });
+    });
+
+    describe('loading from URI string', function() {
+      it('invokes parse:uri callback if it given', function(done) {
+        var loader = LoaderFactory({ 'parse:uri': function(text) {
+          expect(text).to.be('test');
+          return { f1: 'f1t' };
+        }});
+        loader('test').done(function(obj) {
+          expect(obj).to.eql({ f1: 'f1t' });
+          done();
+        });
+      });
+
+      it('resolves as string on wrong URI', function(done) {
+        LoaderFactory()('url(wrong.uri)').done(function(obj) {
+          expect(obj).to.eql('url(wrong.uri)');
+          done();
+        });
+      });
+
+      it('resolves as content on wrong content of downloaded file', function(done) {
+        LoaderFactory()('url("data/wrong-data.txt")').done(function(obj) {
+          expect(obj).to.eql('wrong data\n');
           done();
         });
       });
 
       it('load valid HTML file', function(done) {
-        LoaderFactory()('data/data.html').done(function(obj) {
+        LoaderFactory()('url( "data/data.html" )').done(function(obj) {
           expect(obj).to.eql({ f1: 'f1t', f2: 'f2t' });
           done();
         });
       });
 
       it('load valid JSON file', function(done) {
-        LoaderFactory()('data/data.json').done(function(obj) {
+        LoaderFactory()("url('data/data.json')").done(function(obj) {
           expect(obj).to.eql({ f1: 'f1t', f2: 'f2t' });
           done();
         });
@@ -132,8 +163,21 @@
     });
 
     describe('loading from jQuery', function() {
+      var jquery = $('<i id="f1">f1t</i><i id="f2">f2t</i>');
+
+      it('invokes parse:jquery callback if it given', function(done) {
+        var loader = LoaderFactory({ 'parse:jquery': function(obj) {
+          expect(obj).to.eql(jquery);
+          return { f1: 'f1t' };
+        }});
+        loader(jquery).done(function(obj) {
+          expect(obj).to.eql({ f1: 'f1t' });
+          done();
+        });
+      });
+
       it('loads from jQuery', function(done) {
-        LoaderFactory()($('<i id="f1">f1t</i><i id="f2">f2t</i>')).done(function(obj) {
+        LoaderFactory()(jquery).done(function(obj) {
           expect(obj).to.eql({ f1: 'f1t', f2: 'f2t' });
           done();
         });
@@ -141,7 +185,7 @@
     });
 
     describe('loading from plain object', function() {
-      it('loads from jQuery', function(done) {
+      it('loads from plain object', function(done) {
         LoaderFactory()({ f1: 'f1t', f2: 'f2t' }).done(function(obj) {
           expect(obj).to.eql({ f1: 'f1t', f2: 'f2t' });
           done();
